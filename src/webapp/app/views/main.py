c30ab489db43563c1506745da58668cad17f5c85
gokul-krishna <gg@usfca.edu>
2019-04-09 21:29:58 -0700

    fixed static files problem

--- src/webapp/app/views/main.py
+++ src/webapp/app/views/main.py
@@ -4,34 +4,38 @@ import random
 from flask import send_from_directory
 import os
 
+
+root_dir = os.path.dirname(os.getcwd()) + '/webapp/app/'
+
+
 @app.route('/js/<path:filename>')
 def serve_js(filename):
-    root_dir = os.path.dirname(os.getcwd())
+    print(os.path.join(root_dir, 'static', 'js'))
     return send_from_directory(os.path.join(root_dir, 'static', 'js'), filename)
+
 
 @app.route('/images/<path:filename>')
 def serve_images(filename):
-    root_dir = os.path.dirname(os.getcwd())
     return send_from_directory(os.path.join(root_dir, 'static', 'images_2'), filename)
+
 
 @app.route('/images_2/<path:filename>')
 def serve_images_2(filename):
-    root_dir = os.path.dirname(os.getcwd())
     return send_from_directory(os.path.join(root_dir, 'static', 'images_2'), filename)
+
 
 @app.route('/plugins/<path:filename>')
 def serve_plugins(filename):
-    root_dir = os.path.dirname(os.getcwd())
     return send_from_directory(os.path.join(root_dir, 'static', 'plugins'), filename)
+
 
 @app.route('/styles/<path:filename>')
 def serve_styles(filename):
-    root_dir = os.path.dirname(os.getcwd())
     return send_from_directory(os.path.join(root_dir, 'static', 'styles'), filename)
+
 
 @app.route('/css/<path:filename>')
 def serve_css(filename):
-    root_dir = os.path.dirname(os.getcwd())
     return send_from_directory(os.path.join(root_dir, 'static', 'css'), filename)
 
 
