5486487c34415e8d340c39396e5950663c52cde6
jyotipmahes <manish1352@gmail.com>
2019-04-09 21:14:12 -0700

    Web app updated

--- src/webapp/app/views/main.py
+++ src/webapp/app/views/main.py
@@ -1,12 +1,48 @@
 from flask import render_template, jsonify
 from app import app
 import random
+from flask import send_from_directory
+import os
+
+@app.route('/js/<path:filename>')
+def serve_js(filename):
+    root_dir = os.path.dirname(os.getcwd())
+    return send_from_directory(os.path.join(root_dir, 'static', 'js'), filename)
+
+@app.route('/images/<path:filename>')
+def serve_images(filename):
+    root_dir = os.path.dirname(os.getcwd())
+    return send_from_directory(os.path.join(root_dir, 'static', 'images_2'), filename)
+
+@app.route('/images_2/<path:filename>')
+def serve_images_2(filename):
+    root_dir = os.path.dirname(os.getcwd())
+    return send_from_directory(os.path.join(root_dir, 'static', 'images_2'), filename)
+
+@app.route('/plugins/<path:filename>')
+def serve_plugins(filename):
+    root_dir = os.path.dirname(os.getcwd())
+    return send_from_directory(os.path.join(root_dir, 'static', 'plugins'), filename)
+
+@app.route('/styles/<path:filename>')
+def serve_styles(filename):
+    root_dir = os.path.dirname(os.getcwd())
+    return send_from_directory(os.path.join(root_dir, 'static', 'styles'), filename)
+
+@app.route('/css/<path:filename>')
+def serve_css(filename):
+    root_dir = os.path.dirname(os.getcwd())
+    return send_from_directory(os.path.join(root_dir, 'static', 'css'), filename)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
 
 
 @app.route('/map')
@@ -25,3 +61,4 @@ def map_refresh():
 @app.route('/contact')
 def contact():
     return render_template('contact.html', title='Contact')
+
