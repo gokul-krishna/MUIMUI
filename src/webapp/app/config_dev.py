e97639e70c0b0357bbf69523fe5764139c9c1a9f
gokul-krishna <gg@usfca.edu>
2019-04-13 13:08:54 -0700

    bug

--- src/webapp/app/config_dev.py
+++ src/webapp/app/config_dev.py
@@ -3,8 +3,7 @@ import logging
 from app.config_common import *
 
 
-# DEBUG can only be set to True in a development environment for
-security reasons
+# DEBUG can only be set to True in a development environment for security reasons
 DEBUG = True
 
 # Secret key for generating tokens
